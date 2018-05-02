2542a4d29d6ef5d8bbe50aa52ab0665f58e35443
storrellas <storelllas@gmail.com>
2018-05-02 17:01:59 +0200

    Removed unused items

--- combiner/urls.py
+++ combiner/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^sample/', views.SampleView.as_view()),
-    url('', views.IndexView.as_view()),
+    url('', views.SampleView.as_view()),
 ]
