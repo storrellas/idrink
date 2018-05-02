8aea13bc2deda122103d61f225c81c4d5b9d87c2
storrellas <storrellas@localhost.localdomain>
2018-05-02 14:59:13 +0200

    ENH: Using templates

--- combiner/views.py
+++ combiner/views.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(TemplateView):
+    template_name = "index.html"
