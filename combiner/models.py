d185a35227256d0167e7b840823ef139103f2587
TORRELLAS SOCASTRO Sergi <sergi.torrellassocastro@soprasteria.com>
2018-05-15 13:56:32 +0200

    ENH: Added mongoengine

--- combiner/models.py
+++ combiner/models.py
@@ -14,3 +14,18 @@ class DrinkMongo(Document):
     name = fields.StringField(required=True)
     description = fields.StringField(required=True, null=True)
     ingredient_list = fields.ListField(fields.ReferenceField(Ingredient))
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+
+class Drink(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+
+
+class DrinkIngredient(models.Model):
+    drink = models.ForeignKey(Drink)
+    ingredient = models.ForeignKey(Ingredient)
+    size = models.IntegerField()
