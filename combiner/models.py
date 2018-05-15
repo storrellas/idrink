84e4164e6806b95943d52d2425e304c3dc278c45
TORRELLAS SOCASTRO Sergi <sergi.torrellassocastro@soprasteria.com>
2018-05-15 14:18:12 +0200

    ENH: Added traditional SQLite models

--- combiner/models.py
+++ combiner/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+"""
 from mongoengine import *
 
 class IngredientMongo(Document):
@@ -14,7 +15,7 @@ class DrinkMongo(Document):
     name = fields.StringField(required=True)
     description = fields.StringField(required=True, null=True)
     ingredient_list = fields.ListField(fields.ReferenceField(Ingredient))
-
+"""
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=30)
